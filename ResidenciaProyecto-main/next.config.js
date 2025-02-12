b1e73b3a879bb63d6629ea24d4b5227b5afa6dee
migueszitle111 <miguel.zitle193@tectijuana.edu.mx>
2025-02-11 17:16:15 -0800

    cambios

--- ResidenciaProyecto-main/next.config.js
+++ ResidenciaProyecto-main/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
       domains: ['res.cloudinary.com'],
     },
+    trailingSlash: true, // Asegura que Netlify maneje las rutas correctamente
     output: "export", // Esto ayuda en Netlify a generar páginas estáticas
   }
   
